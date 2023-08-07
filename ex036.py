34194781a8f9b4a33da2ef27ff6a9937defcbfa7
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-08-06 23:31:28 -0300

    feat: "ex036"

---
+++ ex036.py
@@ -0,0 +1,9 @@
+casa = float(input("Valor da casa: R$"))
+salario = float(input("Salario do   comprador: R$"))
+anos = int(input('Quantos anos de financimento?'))
+prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa, anos), end='')
+print('A prestação será de R${:.2f}.format(prestação)')
+
+print('COMPRANDO tem que pagar {} e o {}'.format(prestacao, minimo))
